df12da57ce727363feb2b9f0a46971b0ba94923c
Nikolaj Tolkaciov <nikolaj.tolkaciov@devbridge.com>
2018-08-01 18:26:09 +0300

    Fix indent and other small issue

--- libs/utils.js
+++ libs/utils.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
 module.exports = {
     generateStub : function () {
+        let somethingChanged = false;
         if (!fs.existsSync('reports')) {
             fs.mkdirSync('reports');
+            somethingChanged = true;
         }
         if (!fs.existsSync('screenshots')) {
             fs.mkdirSync('screenshots');
+            somethingChanged = true;
         }
         if (!fs.existsSync('./libs/credentials.js')) {
             fs.writeFile('./libs/credentials.js', 'module.exports = {\n\t"user":"",\n\t"password":""\n}');
+            somethingChanged = true;
+        }
+        if(somethingChanged) {
             console.log("Stub generated!\n");
         }
     }    
