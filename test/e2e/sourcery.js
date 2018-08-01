2c6ea42ca8692f556bfbb31226ba3f8b1d816722
Nikolaj Tolkaciov <nikolaj.tolkaciov@devbridge.com>
2018-08-01 18:14:27 +0300

    Implementation of POM

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -1,31 +1,35 @@
 var conf = require('../../nightwatch.conf.js');
+var common= require('../../obj/common');
+var login = require('../../obj/login');
+var timeLogging = require('../../obj/timeLogging');
 
 module.exports = {
   'Login to sourcebooks': function (browser) {
-    browser
-    .url(browser.launchUrl)
-    .waitForElementVisible('h1'); // wait for the Login title
-    browser.element('css selector', '.Select-placeholder', function(result) {
-        if(result.status != -1) { 
-            browser.click('.Select-placeholder')
-        }
-    });
-    browser.element('css selector', '[aria-label="Demo User"]', function(result) {
-        if(result.status != -1) { 
-            browser.click('css selector', '[aria-label="Demo User"]')
-        }
-    });
-    browser.assert.containsText('.Select-value-label', 'Demo User');
-    browser.element('css selector', '[type="submit"]', function(result) {
-        if(result.status != -1) {
-            browser
-            .click('css selector', '[type="submit"]')
-            .waitForElementVisible('.user-info__title');
-        }
-    });
+        let userUnderTest = "Demo User";
 
-    browser.assert.containsText('.user-info__title', 'Demo User') // assert body contains text
-        .saveScreenshot(conf.imgpath(browser) + 'Demo.png')
-        .end();
+        browser
+        .url(browser.launchUrl)
+        .waitForElementVisible(common.pageTitle);
+        browser.element('css selector', login.userSelect, function(result) {
+            if(result.status != -1) { 
+                browser.click(login.userSelect)
+            }
+        });
+        browser.element('css selector', login.getSpecificSelectUserOption(userUnderTest), function(result) {
+            if(result.status != -1) { 
+                browser.click(login.getSpecificSelectUserOption(userUnderTest))
+            }
+        });
+        browser.assert.containsText(login.userSelectedValue, userUnderTest);
+        browser.element('css selector', common.submitButton, function(result) {
+            if(result.status != -1) {
+                browser
+                .click(common.submitButton)
+                .waitForElementVisible(timeLogging.loggedInUsersName);
+            }
+        });
+        browser.assert.containsText(timeLogging.loggedInUsersName, userUnderTest)
+            .saveScreenshot(conf.imgpath(browser) + 'Demo.png')
+            .end();
     }
-  };
+};
