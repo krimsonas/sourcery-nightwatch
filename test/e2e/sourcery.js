4874c544fbb54a9bef143b1afecee12ec5148730
Nikolaj Tolkaciov <nikolaj.tolkaciov@devbridge.com>
2018-08-06 17:32:50 +0300

    Final refactoring

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -1,35 +1,34 @@
-var conf = require('../../nightwatch.conf.js');
-var common= require('../../obj/common');
-var login = require('../../obj/login');
-var timeLogging = require('../../obj/timeLogging');
+const c = require('../../libs/constants');
+const common= require('../../obj/common');
+const login = require('../../obj/login');
+const timeLogging = require('../../obj/timeLogging');
 
 module.exports = {
-  'Login to sourcebooks': function (browser) {
+    'Login to sourcebooks': function (browser) {
         let userUnderTest = "Demo User";
 
         browser
         .url(browser.launchUrl)
         .waitForElementVisible(common.pageTitle)
-        .element('css selector', login.userSelect, function(result) {
-            if(result.status != -1) { 
+        .isVisible(login.userSelect, function(result) {
+            if(result.status === c.ELEMENT_FOUND) { 
                 browser.click(login.userSelect)
             }
         })
-        .element('css selector', login.getSpecificSelectUserOption(userUnderTest), function(result) {
-            if(result.status != -1) { 
+        .isVisible(login.getSpecificSelectUserOption(userUnderTest), function(result) {
+            if(result.status === c.ELEMENT_FOUND) { 
                 browser.click(login.getSpecificSelectUserOption(userUnderTest))
             }
         })
         .assert.containsText(login.userSelectedValue, userUnderTest)
-        .element('css selector', common.submitButton, function(result) {
-            if(result.status != -1) {
+        .isVisible(common.submitButton, function(result) {
+            if(result.status === c.ELEMENT_FOUND) {
                 browser
                 .click(common.submitButton)
                 .waitForElementVisible(timeLogging.loggedInUsersName);
             }
         })
         .assert.containsText(timeLogging.loggedInUsersName, userUnderTest)
-            .saveScreenshot(conf.imgpath(browser) + 'Demo.png')
-            .end();
+        .end();
     }
 };
