5b84ffe1801526d79fd91771f007ead15d060e80
Nikolaj Tolkaciov <nikolaj.tolkaciov@devbridge.com>
2018-08-06 17:09:24 +0300

    Update login test case to include role locator

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -5,17 +5,35 @@ module.exports = {
         browser
         .url(browser.launchUrl)
         .waitForElementVisible('h1'); // wait for the Login title
-        browser.element('css selector', '.Select-placeholder', function(result) {
+        //Click to expand select user dropdown
+        browser.element('css selector', '#react-select-2--value', function(result) {
             if(result.status != -1) { 
-                browser.click('.Select-placeholder')
+                browser.click('#react-select-2--value');
             }
         });
+        //Select from expanded droprown
         browser.element('css selector', '[aria-label="Demo User"]', function(result) {
             if(result.status != -1) { 
-                browser.click('css selector', '[aria-label="Demo User"]')
+                browser.click('css selector', '[aria-label="Demo User"]');
             }
         });
-        browser.assert.containsText('.Select-value-label', 'Demo User');
+        //Assert value is selected
+        browser.assert.containsText('#react-select-2--value-item', 'Demo User');
+        //Click to expand select role dropdown
+        browser.element('css selector', '#react-select-3--value', function(result) {
+            if(result.status != -1) { 
+                browser.click('css selector', '#react-select-3--value');
+            }
+        });
+        //Select from expanded droprown
+        browser.element('css selector', '[aria-label="Admin"]', function(result) {
+            if(result.status != -1) { 
+                browser.click('css selector', '[aria-label="Admin"]');
+            }
+        });
+        //Assert value is selected
+        browser.assert.containsText('#react-select-3--value-item', 'Admin');
+        //Click submit button
         browser.element('css selector', '[type="submit"]', function(result) {
             if(result.status != -1) {
                 browser
@@ -23,7 +41,8 @@ module.exports = {
                 .waitForElementVisible('.user-info__title');
             }
         });
-        browser.assert.containsText('.user-info__title', 'Demo User') // assert body contains text
+        //Assert if expected user is logged in
+        browser.assert.containsText('.user-info__title', 'Demo User')
             .saveScreenshot(conf.imgpath(browser) + 'Demo.png')
             .end();
     }
