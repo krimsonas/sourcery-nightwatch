17fa963da859018e313fbaf7065dc8152c3148a7
DESKTOP-4EL9I4R\David <deividas.vaskevicius@ktu.edu>
2018-08-07 16:00:55 +0300

    fixed spacing, improved readability

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -46,19 +46,18 @@ module.exports = {
         browser.assert.containsText('.user-info__title', 'Deividas Vaškevicius')
             .saveScreenshot(conf.imgpath(browser) + 'Demo.png');
 
-        browser.assert.containsText('.main-nav', 'Time Logging');
-        browser.assert.containsText('.main-nav', 'Invoices');
-        browser.assert.containsText('.main-nav', 'Projects');
-        browser.assert.containsText('.main-nav', 'Clients');
-        browser.assert.containsText('.main-nav', 'Time Entries');
+        browser.assert.containsText('.main-nav', 'Time Logging')
+        .assert.containsText('.main-nav', 'Invoices')
+        .assert.containsText('.main-nav', 'Projects')
+        .assert.containsText('.main-nav', 'Clients')
+        .assert.containsText('.main-nav', 'Time Entries')
+        .assert.containsText('.main-nav', 'Tasks');
         
-        browser.assert.containsText('.main-nav__link.main-nav__link--active', 'Time Logging');
-        browser.expect.element('.main-nav__link--active').to.have.css('color').which.equals('rgba(64, 76, 237, 1)');
+        browser.assert.containsText('.main-nav__link--active', 'Time Logging')
+        .assert.cssProperty('.main-nav__link.main-nav__link--active', 'color', 'rgba(64, 76, 237, 1)')
         
         var d = new Date();
-        browser.assert.containsText('.calendar__day.calendar--today.calendar--selected.calendar__day', d.getDate());
-
-
+        browser.assert.containsText('.calendar--selected', d.getDate());
 
         browser.end();
     }
