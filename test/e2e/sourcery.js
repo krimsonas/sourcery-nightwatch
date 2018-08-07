a38dff1318de5225d9c138307e2725518a56db4f
Ruta Laurikaityte <ruta.laurikaityte@gmail.com>
2018-08-07 15:43:03 +0300

    Updated code according test case No. 1

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -12,13 +12,13 @@ module.exports = {
             }
         });
         //Select from expanded droprown
-        browser.element('css selector', '[aria-label="Demo User"]', function(result) {
+        browser.element('css selector', '[aria-label="Ruta Laurikaityte"]', function(result) {
             if(result.status != -1) { 
-                browser.click('css selector', '[aria-label="Demo User"]');
+                browser.click('css selector', '[aria-label="Ruta Laurikaityte"]');
             }
         });
         //Assert value is selected
-        browser.assert.containsText('#react-select-2--value-item', 'Demo User');
+        browser.assert.containsText('#react-select-2--value-item', 'Ruta Laurikaityte');
         //Click to expand select role dropdown
         browser.element('css selector', '#react-select-3--value', function(result) {
             if(result.status != -1) { 
@@ -42,8 +42,18 @@ module.exports = {
             }
         });
         //Assert if expected user is logged in
-        browser.assert.containsText('.user-info__title', 'Demo User')
-            .saveScreenshot(conf.imgpath(browser) + 'Demo.png')
-            .end();
+        browser.assert.containsText('.user-info__title', 'Ruta Laurikaityte')
+            .saveScreenshot(conf.imgpath(browser) + 'Demo.png');
+            
+        browser.assert.containsText('.calendar--today','7');
+        browser.assert.containsText('[href="/time-logging"]','Time Logging');
+        browser.assert.containsText('[href="/invoices"]','Invoices');
+        browser.assert.containsText('[href="/tasks"]','Tasks');
+        browser.assert.containsText('[href="/projects"]','Projects');
+        browser.assert.containsText('[href="/clients"]','Clients');
+        browser.assert.containsText('[href="/time-entries"]','Time Entries');
+        browser.assert.containsText('.main-nav__link--active','Time Logging');
+        browser.assert.cssProperty('.main-nav__link--active','color', 'rgba(64, 76, 237, 1)')
+        .end();
     }
 };
