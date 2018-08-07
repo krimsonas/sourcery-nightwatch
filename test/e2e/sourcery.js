49961bbe3ef48dc187533656ed4a449a4508dc26
Akvile Jarmalaviciute <akvile.jarmalaviciute@devbridge.com>
2018-08-07 16:01:57 +0300

    Updated admin login

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -44,24 +44,15 @@ module.exports = {
         //Assert if expected user is logged in
         browser.assert.containsText('.user-info__title', 'Akvile Jarmalaviciute')
             .saveScreenshot(conf.imgpath(browser) + 'Demo.png');
-        //Select current date
-        /*
-        browser.element('css selector', '[.calendar__day calendar--today calendar--selected= "7"]', function(result){
-            if(result.status != -1) {
-                browser.click('css selector', '[.calendar__day calendar--today calendar--selected= "7"]');
-            }
-        });   
-        */
-        browser.assert.containsText('.calendar--today', '7');
-        browser.assert.containsText('.main-nav', 'Time Logging');
-        browser.assert.containsText('.main-nav', 'Invoices');
-        browser.assert.containsText('.main-nav', 'Tasks');
-        browser.assert.containsText('.main-nav', 'Projects');
-        browser.assert.containsText('.main-nav', 'Clients');
-        browser.assert.containsText('.main-nav', 'Time Entries');
-        browser.assert.containsText('[href="/time-entries"]', 'Time Entries');
-        browser.assert.containsText('.main-nav__link--active', 'Time Logging');
-        browser.assert.cssProperty('.main-nav__link--active', 'color', 'rgba(64, 76, 237, 1)')
+       
+        browser.assert.containsText('.main-nav', 'Time Logging')
+          .assert.containsText('.main-nav', 'Invoices')
+          .assert.containsText('.main-nav', 'Tasks')
+          .assert.containsText('.main-nav', 'Projects')
+          .assert.containsText('.main-nav', 'Clients')
+          .assert.containsText('.main-nav', 'Time Entries')
+          .assert.containsText('.main-nav__link--active', 'Time Logging')
+          .assert.cssProperty('.main-nav__link--active', 'color', 'rgba(64, 76, 237, 1)')
 
         .end();       
     }
