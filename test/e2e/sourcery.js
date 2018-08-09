361d57dbd6486ca20645dee2206163b4aad496ec
AzureAD\TautvydasDirmeikis <tautvydas.dirmeikis@devbridge.com>
2018-08-09 14:01:59 +0300

    Finished the feature.

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -12,13 +12,13 @@ module.exports = {
             }
         });
         //Select from expanded droprown
-        browser.element('css selector', '[aria-label="Demo User"]', function(result) {
+        browser.element('css selector', '[aria-label="Tautvydas Dirmeikis"]', function(result) {
             if(result.status != -1) { 
-                browser.click('css selector', '[aria-label="Demo User"]');
+                browser.click('css selector', '[aria-label="Tautvydas Dirmeikis"]');
             }
         });
         //Assert value is selected
-        browser.assert.containsText('#react-select-2--value-item', 'Demo User');
+        browser.assert.containsText('#react-select-2--value-item', 'Tautvydas Dirmeikis');
         //Click to expand select role dropdown
         browser.element('css selector', '#react-select-3--value', function(result) {
             if(result.status != -1) { 
@@ -42,8 +42,22 @@ module.exports = {
             }
         });
         //Assert if expected user is logged in
-        browser.assert.containsText('.user-info__title', 'Demo User')
-            .saveScreenshot(conf.imgpath(browser) + 'Demo.png')
-            .end();
+        browser.assert.containsText('.user-info__title', 'Tautvydas Dirmeikis');
+
+        //Assert buttons are existing in menu
+        browser.assert.containsText('.main-nav', 'Time Logging');
+        browser.assert.containsText('.main-nav', 'Invoices');
+        browser.assert.containsText('.main-nav', 'Projects');
+        browser.assert.containsText('.main-nav', 'Clients');
+        browser.assert.containsText('.main-nav', 'Tasks');
+        browser.assert.containsText('a[href="/time-entries"]','Time Entries');
+
+        //Assert "Time logging" is active and verify its color
+        browser.assert.containsText('.main-nav__link--active', 'Time Logging');
+        browser.assert.cssProperty('a[href="/time-logging"]', 'color', 'rgba(64, 76, 237, 1)');
+
+        browser.saveScreenshot(conf.imgpath(browser) + 'Demo.png')
+        browser.end();
+
     }
 };
