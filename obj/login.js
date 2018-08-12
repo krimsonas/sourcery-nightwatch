5186f89a26cc2bd723cea0c0c759fad362cfa260
Giedrius Saulenas <giedrius.saulenas@gmail.com>
2018-08-12 23:33:33 +0300

    Login with POM

--- obj/login.js
+++ obj/login.js
@@ -2,8 +2,8 @@ module.exports = {
     userSelect : '.Select-placeholder',
     userSelectedValue : '.Select-value-label',
     userSelectListValue : '[aria-label="?"]',
-    roleSelect : '.Select has-value is-searchable Select--single',
-    roleSelectedValue : '#react-select-3--value-item',
+    roleSelect : '[id="react-select-3--value-item"]',
+    roleSelectedValue : '[id="react-select-3--value-item"]',
     roleSelectListValue : '[aria-label="?"]',
     getSpecificSelectUserOption : function(userName) {
         return this.userSelectListValue.replace('?', userName);
--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -7,7 +7,7 @@ const timeLogging = require('../../obj/timeLogging');
 module.exports = {
     'Login to sourcebooks': function (browser) {
         let userUnderTest = "Giedrius Saulenas";
-        let roleUnderTest = "User";
+        let roleUnderTest = "Admin";
         browser
         
         .url(browser.launchUrl)
@@ -18,60 +18,35 @@ module.exports = {
                 browser.click(login.userSelect)
             }
         })
-        /*
-        browser.element('css selector', '#react-select-2--value', function(result) {
-            if(result.status != -1) { 
-                browser.click('#react-select-2--value');
-            }
-        });
-        */
+
+        //Select from expanded droprown
         .isVisible(login.getSpecificSelectUserOption(userUnderTest), function(result) {
             if (result.status === c.ELEMENT_FOUND){
                 browser.click(login.getSpecificSelectUserOption(userUnderTest))
             }
         })
+
+        //Checks if user that we want to select is selected from the list
         .assert.containsText(login.userSelectedValue, userUnderTest)
-        /*
-        //Select from expanded droprown
-        browser.element('css selector', '[aria-label="Demo User"]', function(result) {
-            if(result.status != -1) { 
-                browser.click('css selector', '[aria-label="Demo User"]');
-            }
-        });
-        */
-    
+
+        //Click to expand select role dropdown
         .isVisible(login.roleSelect, function(result) {
             if(result.status === c.ELEMENT_FOUND){
                 browser.click(login.roleSelect)
             }
         })
-        /*
-        //Assert value is selected
-        browser.assert.containsText('#react-select-2--value-item', 'Demo User');
-        //Click to expand select role dropdown
-        browser.element('css selector', '#react-select-3--value', function(result) {
-            if(result.status != -1) { 
-                browser.click('css selector', '#react-select-3--value');
-            }
-        });
-        */
-        //Select from expanded droprown
+    
+        //Select from expanded dropdown
         .isVisible(login.getSpecificSelectRoleOption(roleUnderTest), function(result) {
             if (result.status === c.ELEMENT_FOUND){
                 browser.click(login.getSpecificSelectRoleOption(roleUnderTest))
             }
         })
+
+        //Checks if role that we want to select is selected from the list
         .assert.containsText(login.roleSelectedValue, roleUnderTest)
-        /*
-        browser.element('css selector', '[aria-label="Admin"]', function(result) {
-            if(result.status != -1) { 
-                browser.click('css selector', '[aria-label="Admin"]');
-            }
-        });
-        //Assert value is selected
-        browser.assert.containsText('#react-select-3--value-item', 'Admin');
+       
         //Click submit button
-        */
         .isVisible(common.submitButton, function(result) {
             if (result.status === c.ELEMENT_FOUND){
                 browser
@@ -79,19 +54,9 @@ module.exports = {
                 .waitForElementVisible(timeLogging.loggedInUsersName)
             }
         })
-        .assert.containsText(timeLogging.loggedInUsersName, userUnderTest)
-        /*
-        browser.element('css selector', '[type="submit"]', function(result) {
-            if(result.status != -1) {
-                browser
-                .click('css selector', '[type="submit"]')
-                .waitForElementVisible('.user-info__title');
-            }
-        });
-        //Assert if expected user is logged in
         
-        browser.assert.containsText('.user-info__title', 'Demo User')
-            .saveScreenshot(conf.imgpath(browser) + 'Demo.png')*/
-            .end();
+        //Assert if expected user is logged in
+        .assert.containsText(timeLogging.loggedInUsersName, userUnderTest)
+        .end();
     }
 };
