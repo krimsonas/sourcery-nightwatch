a9f1b72577dd3cb4f8db0d84f9d13f04af716003
Ruta Laurikaityte <ruta.laurikaityte@gmail.com>
2018-08-12 20:35:52 +0300

    Admin creates new task

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -45,15 +45,102 @@ module.exports = {
         browser.assert.containsText('.user-info__title', 'Ruta Laurikaityte')
             .saveScreenshot(conf.imgpath(browser) + 'Demo.png');
             
-        browser.assert.containsText('.calendar--today','7');
-        browser.assert.containsText('[href="/time-logging"]','Time Logging');
-        browser.assert.containsText('[href="/invoices"]','Invoices');
-        browser.assert.containsText('[href="/tasks"]','Tasks');
-        browser.assert.containsText('[href="/projects"]','Projects');
-        browser.assert.containsText('[href="/clients"]','Clients');
-        browser.assert.containsText('[href="/time-entries"]','Time Entries');
-        browser.assert.containsText('.main-nav__link--active','Time Logging');
-        browser.assert.cssProperty('.main-nav__link--active','color', 'rgba(64, 76, 237, 1)')
+ 
+        //Click menu item "Tasks"
+        browser.element('css selector', '[href="/tasks"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[href="/tasks"]')
+                .waitForElementVisible('.main-nav__link--active', 5000);
+            }
+        });
+        //Assert value is selected
+        browser.assert.containsText('.main-nav__link--active', 'Tasks')
+        .saveScreenshot(conf.imgpath(browser) + 'TasksPage.png');
+
+        
+        //Click button "Create Task"
+        browser.element('css selector', '[type="button"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[type="button"]')
+                .waitForElementVisible('h1');
+            }
+        });
+
+        //Fill field "Task Name"
+        browser.element('css selector', '[name="taskDetailsForm.name"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[name="taskDetailsForm.name"]')
+                .setValue('input[name="taskDetailsForm.name"]', 'Sinus1');
+            }
+        });
+
+        //Fill field "Description"
+        browser.element('css selector', '[name="taskDetailsForm.description"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[name="taskDetailsForm.description"]')
+                .setValue('textarea[name="taskDetailsForm.description"]', 'testarea');
+            }
+        });
+
+        //Click to expand select user dropdown "Bill to Client"
+        browser.element('css selector', '#react-select-6--value', function(result) {
+            if(result.status != -1) { 
+                browser.click('#react-select-6--value');
+            }
+        });
+        //Select from expanded droprown value YES
+        browser.element('css selector', '[aria-label = "Yes"]', function(result) {
+            if(result.status != -1) { 
+                browser.click('css selector', '[aria-label = "Yes"]');
+            }
+        });
+        //Assert value is selected
+        browser.assert.containsText('#react-select-6--value', 'Yes');
+         
+        //Fill field "Hourly Rate"
+        browser.element('css selector', '[name="taskDetailsForm.rate"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[name="taskDetailsForm.rate"]')
+                .setValue('input[name="taskDetailsForm.rate"]', '2')
+                .saveScreenshot(conf.imgpath(browser) + 'CreateTask.png')
+            }
+        });
+
+        //Click Save button
+        browser.element('css selector', '[type="submit"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[type="submit"]')
+            }
+        });
+
+        //Click menu item "Tasks"
+        browser.element('css selector', '[href="/tasks"]', function(result) {
+            if(result.status != -1) {
+            browser
+            .click('css selector', '[href="/tasks"]')
+            .waitForElementVisible('.main-nav__link--active', 5000);
+            }
+        });
+
+        //Fill field "Task Name"
+        browser.element('css selector', '[type="text"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[type="text"]')
+                .setValue('input[type="text"]', 'Sinus1')
+                .waitForElementVisible('[title="Sinus1"]', 5000)
+                .assert.containsText('.ellipsis', 'Sinus1')
+                .saveScreenshot(conf.imgpath(browser) + 'CreateTask1.png');
+            }
+        })
+
+
         .end();
     }
 };
