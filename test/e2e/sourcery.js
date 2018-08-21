cc4ec0c2aa2b8b44584295fd09ce953bbe516a77
Rimantas Andrulevicius <andrulevicius.r@gmail.com>
2018-08-21 16:13:19 +0300

    Login function to page object model. Code has too many comments

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -1,48 +1,70 @@
 var conf = require('../../nightwatch.conf.js');
+const c = require('../../libs/constants.js')
+const common = require('../../obj/common.js');
+const login = require('../../obj/login.js');
+const timeLogging = require('../../obj/timeLogging.js');
 
 module.exports = {
     'Login to sourcebooks': function (browser) {
+
+        let currentUser = "Demo User";
+        let currentUserRole = "Admin";
+        //---------------------------------------------------------------------------------------------------
+        //click into User field
+        //find your user and select it
+        //=>assert
+        //---------------------------------------------------------------------------------------------------
         browser
         .url(browser.launchUrl)
-        .waitForElementVisible('h1'); // wait for the Login title
+        .waitForElementVisible(common.pageTitle); // wait for the Login title
         //Click to expand select user dropdown
-        browser.element('css selector', '#react-select-2--value', function(result) {
-            if(result.status != -1) { 
-                browser.click('#react-select-2--value');
+        browser.element('css selector',login.userSelect , function(result) {
+            if(result.status === c.ELEMENT_FOUND) { 
+                browser.click(login.userSelect);
             }
         });
         //Select from expanded droprown
-        browser.element('css selector', '[aria-label="Demo User"]', function(result) {
-            if(result.status != -1) { 
-                browser.click('css selector', '[aria-label="Demo User"]');
+        browser.element('css selector', login.getSpecificSelectUserOption(currentUser), function(result) {
+            if(result.status === c.ELEMENT_FOUND) { 
+                browser.click('css selector', login.getSpecificSelectUserOption(currentUser));
             }
         });
         //Assert value is selected
-        browser.assert.containsText('#react-select-2--value-item', 'Demo User');
+        browser.assert.containsText(login.userSelectedValue, currentUser);
+        //---------------------------------------------------------------------------------------------------
+        //click into role field
+        //find your role and select it
+        //=>assert
+        //---------------------------------------------------------------------------------------------------
         //Click to expand select role dropdown
-        browser.element('css selector', '#react-select-3--value', function(result) {
-            if(result.status != -1) { 
-                browser.click('css selector', '#react-select-3--value');
+        browser.element('css selector', login.userRoleSelectField, function(result) {
+            if(result.status === c.ELEMENT_FOUND) { 
+                browser.click('css selector', login.userRoleSelectField);
             }
         });
         //Select from expanded droprown
-        browser.element('css selector', '[aria-label="Admin"]', function(result) {
-            if(result.status != -1) { 
-                browser.click('css selector', '[aria-label="Admin"]');
+        browser.element('css selector', login.getSpecificSelectUserRoleOption(currentUserRole), function(result) {
+            if(result.status === c.ELEMENT_FOUND) { 
+                browser.click('css selector', login.getSpecificSelectUserRoleOption(currentUserRole));
             }
         });
         //Assert value is selected
-        browser.assert.containsText('#react-select-3--value-item', 'Admin');
+        browser.assert.containsText(login.userRoleSelectField, currentUserRole);
+        //---------------------------------------------------------------------------------------------------
+        //find login button and click it
+        //user name is in time logging page
+        //=>assert
+        //---------------------------------------------------------------------------------------------------
         //Click submit button
-        browser.element('css selector', '[type="submit"]', function(result) {
-            if(result.status != -1) {
+        browser.element('css selector', common.submitButton, function(result) {
+            if(result.status === c.ELEMENT_FOUND) {
                 browser
-                .click('css selector', '[type="submit"]')
-                .waitForElementVisible('.user-info__title');
+                .click('css selector', common.submitButton)
+                .waitForElementVisible(timeLogging.loggedInUsersName);
             }
         });
         //Assert if expected user is logged in
-        browser.assert.containsText('.user-info__title', 'Demo User')
+        browser.assert.containsText(timeLogging.loggedInUsersName, currentUser)
             .saveScreenshot(conf.imgpath(browser) + 'Demo.png')
             .end();
     }
