a64b32967c79565424edfa49971cb408d3b01898
Ruta Laurikaityte <ruta.laurikaityte@gmail.com>
2018-08-24 20:13:49 +0300

    Admin creates new client

--- test/e2e/sourcery.js
+++ test/e2e/sourcery.js
@@ -12,13 +12,13 @@ module.exports = {
             }
         });
         //Select from expanded droprown
-        browser.element('css selector', '[aria-label="Demo User"]', function(result) {
+        browser.element('css selector', '[aria-label="Ruta Laurikaityte"]', function(result) {
             if(result.status != -1) { 
-                browser.click('css selector', '[aria-label="Demo User"]');
+                browser.click('css selector', '[aria-label="Ruta Laurikaityte"]');
             }
         });
         //Assert value is selected
-        browser.assert.containsText('#react-select-2--value-item', 'Demo User');
+        browser.assert.containsText('#react-select-2--value-item', 'Ruta Laurikaityte');
         //Click to expand select role dropdown
         browser.element('css selector', '#react-select-3--value', function(result) {
             if(result.status != -1) { 
@@ -42,8 +42,97 @@ module.exports = {
             }
         });
         //Assert if expected user is logged in
-        browser.assert.containsText('.user-info__title', 'Demo User')
-            .saveScreenshot(conf.imgpath(browser) + 'Demo.png')
-            .end();
+        browser.assert.containsText('.user-info__title', 'Ruta Laurikaityte')
+            .saveScreenshot(conf.imgpath(browser) + 'Demo.png');
+            
+ 
+        //Click menu item "Clients"
+        browser.element('css selector', '[href="/clients"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[href="/clients"]')
+                .waitForElementVisible('.main-nav__link--active', 5000);
+            }
+        });
+        //Assert value is selected
+        browser.assert.containsText('.main-nav__link--active', 'Clients');
+
+        
+        //Click button "Create Client"
+        browser.element('css selector', '[type="button"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[type="button"]')
+                .waitForElementVisible('h1');
+            }
+        });
+
+        //Fill field "Organization Name"
+        browser.element('css selector', '[name="clientDetailsForm.organization"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[name="clientDetailsForm.organization"]')
+                .setValue('input[name="clientDetailsForm.organization"]', 'Baltic Group AB');
+            }
+        });
+
+        //Fill field "First Name"
+        browser.element('css selector', '[name="clientDetailsForm.contacts[0].firstName"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[name="clientDetailsForm.contacts[0].firstName"]')
+                .setValue('input[name="clientDetailsForm.contacts[0].firstName"]', 'John');
+            }
+        });
+
+        //Fill field "Last Name"
+        browser.element('css selector', '[name="clientDetailsForm.contacts[0].lastName"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[name="clientDetailsForm.contacts[0].lastName"]')
+                .setValue('input[name="clientDetailsForm.contacts[0].lastName"]', 'Smith');
+            }
+        });
+
+        //Fill field "Email"
+        browser.element('css selector', '[name="clientDetailsForm.contacts[0].email"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[name="clientDetailsForm.contacts[0].email"]')
+                .setValue('input[name="clientDetailsForm.contacts[0].email"]', 'John.Smith@balticgroup.com')
+                .saveScreenshot(conf.imgpath(browser) + 'CreateClient.png');
+            }
+        })
+
+        //Click Save button
+        browser.element('css selector', '[type="submit"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[type="submit"]')
+            }
+        });
+
+        //Click menu item "Clients"
+        browser.element('css selector', '[href="/clients"]', function(result) {
+            if(result.status != -1) {
+            browser
+            .click('css selector', '[href="/clients"]')
+            .waitForElementVisible('.main-nav__link--active', 5000);
+            }
+        });
+
+        //Fill field "Task Name"
+        browser.element('css selector', '[type="text"]', function(result) {
+            if(result.status != -1) {
+                browser
+                .click('css selector', '[type="text"]')
+                .setValue('input[type="text"]', 'Baltic Group AB')
+                .waitForElementVisible('[title="Baltic Group AB"]', 5000)
+                .assert.containsText('.ellipsis', 'Baltic Group AB')
+                .saveScreenshot(conf.imgpath(browser) + 'ClientsPage.png');
+            }
+        })
+
+        .end();
     }
 };
